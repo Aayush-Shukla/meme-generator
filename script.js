41066d98a84553ee8df757c2a22a136cc690187d
Ayush Shukla <34869001+Aayush-Shukla@users.noreply.github.com>
2020-09-16 02:59:59 +0530

    fix: different aspect ratio template

--- script.js
+++ script.js
@@ -3,36 +3,38 @@ let ctx=canvas.getContext('2d');
 var memeData;
 var meme=new Image()
 var text=[]
-offsetX=300
+offsetX=canvas.width/2-320
 var selected=-1
+index=0
+var colors=['white','black']
 
-offsetY=50
+offsetY=130
+canvas.style = "position:absolute; left: 50% ;top:130px; margin-left: -320px;";
 
 
-// let response= await fetch('https://api.imgflip.com/get_memes')
+let url='https://api.imgflip.com/get_memes'
 
 
-async function getTemp (){
-let commit= await fetch('https://api.imgflip.com/get_memes')
+async function getImages (){
+let commit= await fetch(url)
   .then((response) => {
     return response.json()
   })
   .then((data) => {
-    // Work with JSON data here
-    // keepIt(data.memes)
+   
     console.log(data)
-    keepIt(data)
+    copyJson(data)
 
-    // console.log(memeData)
+    
   })
   .catch((err) => {
-    // Do something for an error here
+    console.log(err)
   })
 }
 
-getTemp()
+getImages()
 
-function keepIt(memelist){
+function copyJson(memelist){
 
     memeData=JSON.parse(JSON.stringify(memelist))
     console.log(memeData)
@@ -45,25 +47,17 @@ function keepIt(memelist){
 
 
 function init(){
-// console.log("in")
-// ctx.clearRect(0,0,320,320)
-    
-meme.src=memeData.data.memes[3].url
-console.log(meme)
-meme.onload=function(){
-heightmeme=(memeData.data.memes[3].height/memeData.data.memes[3].width)* canvas.width
-console.log(canvas.width,heightmeme)
-ctx.drawImage(meme,0,0,canvas.width,heightmeme)
-}
 
-temptext1=new MemeText('',120,40,'black')
+
+temptext1=new MemeText('',320,40,colors[0])
     text.push(temptext1)
-    temptext2=new MemeText('',120,80,'black')
+    temptext2=new MemeText('',320,500,colors[0])
     text.push(temptext2)
+    DrawMeme()
     
 
 }
-// console.log(memeData)
+
 
 const firstText=document.getElementById("text1")
 
@@ -77,32 +71,10 @@ secondText.addEventListener('input', text2);
 
 function text1(e){
 
-ctx.clearRect(0,0,canvas.width,canvas.height)
-meme.src=memeData.data.memes[3].url
-console.log(meme)
-meme.onload=function(){
-heightmeme=(memeData.data.memes[3].height/memeData.data.memes[3].width)* canvas.width
-console.log(canvas.width,heightmeme)
-ctx.drawImage(meme,0,0,canvas.width,heightmeme)
 
-console.log("in")
-
-    // ctx.font = '30pt Impact';
-    // ctx.textAlign = 'center';
-    // ctx.strokeStyle = 'black';
-    // ctx.lineWidth = 3;
-    // ctx.fillStyle = 'white';
-    
-    // ctx.fillText(e.originalTarget.value, canvas.width / 2, 40);
-    // console.log(e)
-    // temptext=new MemeText(e.originalTarget.value,20,20,'black')
-    // text.push(temptext)
     text[0].updateText(e.originalTarget.value)
-    text[0].draw()
+    DrawMeme()
     console.log(text)
-}
-
-
 }
 
 
@@ -110,38 +82,18 @@ console.log("in")
 
 function text2(e){
 
-    ctx.clearRect(0,0,canvas.width,canvas.height)
-    meme.src=memeData.data.memes[3].url
-    console.log(meme)
-    meme.onload=function(){
-    heightmeme=(memeData.data.memes[3].height/memeData.data.memes[3].width)* canvas.width
-    console.log(canvas.width,heightmeme)
-    ctx.drawImage(meme,0,0,canvas.width,heightmeme)
-    
-    console.log("in")
-    
-        // ctx.font = '30pt Impact';
-        // ctx.textAlign = 'center';
-        // ctx.strokeStyle = 'black';
-        // ctx.lineWidth = 3;
-        // ctx.fillStyle = 'white';
-
-
         text[1].updateText(e.originalTarget.value)
-    text[1].draw()
+        DrawMeme()
         
-        // ctx.fillText(e.originalTarget.value, canvas.width / 2, 340);
-        // console.log(e)
-    }
+ 
+}
     
-    
-    }
 
 
 
+                                                            //constructor
 
-
-    function MemeText(text, x, y, color) {                                                         /*constructor function with 4 paramenter*/
+    function MemeText(text, x, y, color) {                                                         
         this.text = text
         this.x=x
         this.y = y
@@ -154,12 +106,10 @@ function text2(e){
         ctx.textAlign = 'center';
         ctx.strokeStyle = 'black';
         ctx.lineWidth = 3;
-        ctx.fillStyle = 'white';
+        ctx.fillStyle = this.color;
         
         ctx.fillText(this.text, this.x,this.y);
-        // console.log(e)
-
-            
+                    
         }
 
         this.updateText=(txt)=>{
@@ -169,10 +119,11 @@ function text2(e){
         
     }
 
-
+                                                                                // functions for dragging texts
 
     canvas.onmousedown=handleMouseDown
     canvas.onmousemove=handleMouseMove
+    canvas.onmouseup=handleMouseUp
 
 
     function handleMouseDown(e){
@@ -180,7 +131,6 @@ function text2(e){
         startX=parseInt(e.clientX-offsetX);
         startY=parseInt(e.clientY-offsetY);
 
-        // Put your mousedown stuff here
         
         if( distance(startX,startY,text[0].x,text[0].y) < distance(startX,startY,text[1].x,text[1].y))
         {
@@ -190,8 +140,6 @@ function text2(e){
             selected=1
         }
 
-
-        
 
         console.log(startX,startY,selected)
         console.log(distance(startX,startY,text[0].x,text[0].y),distance(startX,startY,text[1].x,text[1].y))
@@ -209,17 +157,76 @@ function text2(e){
         text[selected].x=startX
         text[selected].y=startY
 
-        text[0].draw()
+        DrawMeme()
 
 
 
     }
 
+    function handleMouseUp(e){
+        e.preventDefault();
+        selected=-1;
+      }
 
 
-    function distance(x1, y1, x2, y2) {                                                         /*function to calculate distance b/w two points*/
-        const xDist = x2 - x1
-        const yDist = y2 - y1
+                                                                    // draw func
+    function DrawMeme(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    meme.src=memeData.data.memes[index].url
+    console.log(meme)
+    meme.onload=function(){
+
+
+    if(memeData.data.memes[index].height<=memeData.data.memes[index].width){
+    heightmeme=(memeData.data.memes[index].height/memeData.data.memes[index].width)* canvas.width
+    console.log(canvas.width,heightmeme)
+    ctx.drawImage(meme,0,0,canvas.width,heightmeme)
+    }
+    else{
+
+        widthmeme=(memeData.data.memes[index].width/memeData.data.memes[index].height)* canvas.height
+        
+    ctx.drawImage(meme,0,0,widthmeme,canvas.height)
+
+    }
     
-        return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
+    for(var i=0;i<text.length;i++){
+        text[i].draw()
     }
+
+
+    }
+}
+                                                                // options
+function prev(){
+    if (index>0)
+    {
+    index--}
+    DrawMeme()
+}
+
+
+function next(){
+    if (index<99)
+    {
+    index++}
+    console.log(index)
+    DrawMeme()
+}
+function toggle(){
+    for(var i=0;i<text.length;i++){
+        if (text[i].color==colors[0]){
+            text[i].color=colors[1]
+        }
+        else{
+            text[i].color=colors[0]
+        }
+    }
+    DrawMeme()
+}
+function distance(x1, y1, x2, y2) {                                                         
+    const xDist = x2 - x1
+    const yDist = y2 - y1
+
+    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
+}
